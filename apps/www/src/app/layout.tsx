b61970cca29bb35410277d9ff86bad5405d41b4b
Dmytro <d@klmn.me>
2023-08-15 22:35:48 +0300

    feat: @vercel/analytics

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,5 +1,12 @@
+import { Analytics } from '@vercel/analytics/react'
+
 import 'src/styles/global.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children
+  return (
+    <>
+      {children}
+      <Analytics />
+    </>
+  )
 }
