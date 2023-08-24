9081474d9146f4c50f978e58d19d1a7e5b9b78ca
Dmytro <d@klmn.me>
2023-08-24 16:10:20 +0300

    feat: cleanup Built with page

--- apps/www/src/app/[lang]/built-with/page.tsx
+++ apps/www/src/app/[lang]/built-with/page.tsx
@@ -1,5 +1,4 @@
 import type { Metadata, ResolvingMetadata } from 'next'
-import { Spacer } from '@nextui-org/spacer'
 
 import { USERNAME } from 'my-constants'
 import { availableLocales } from 'my-locales/constants'
@@ -8,8 +7,6 @@ import type { WithLangProp } from 'src/types'
 import { BuiltWith } from 'src/components/BuiltWith'
 import { HomeLink } from 'src/components/HomeLink'
 import { PhotoCard } from 'src/components/PhotoCard'
-// import { SaveLevCard } from 'src/components/SaveLevCard'
-import { SupportUkraineCard } from 'src/components/SupportUkraineCard'
 import { getT } from 'src/utils/getT'
 
 export async function generateMetadata({ params }, parent: ResolvingMetadata) {
@@ -31,22 +28,10 @@ export default function BuiltWithPage({ params }: { params: WithLangProp }) {
   const { lang } = params
   return (
     <div className="flex flex-col max-w-5xl mx-auto gap-4 p-4 relative print:block">
-      <div className="flex flex-col sm:flex-row-reverse print:!flex-col gap-4 relative print:block">
-        <div className="basis-full sm:basis-5/12 sm:sticky print:!relative sm:self-start sm:top-0 print:!top-auto print:max-w-[60%] print:mx-auto print:mt-12">
-          <div className="flex flex-col gap-1 print:block">
-            <PhotoCard lang={lang} />
-            <Spacer />
-            <SupportUkraineCard lang={lang} />
-            {/* <Spacer />
-                <SaveLevCard /> */}
-          </div>
-        </div>
-        <div className="basis-full sm:basis-7/12 print:block">
-          <div>
-            <BuiltWith lang={lang} />
-          </div>
-        </div>
+      <div>
+        <BuiltWith lang={lang} />
       </div>
+
       <div>
         <HomeLink lang={lang} />
       </div>
